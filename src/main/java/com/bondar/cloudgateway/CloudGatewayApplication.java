be031d131a9a9108978432cd03e836df73a34f9c
Ali Bondar <bondarali1380@gmail.com>
2025-01-31 23:40:26 +0330

    add hytrix configuration

--- src/main/java/com/bondar/cloudgateway/CloudGatewayApplication.java
+++ src/main/java/com/bondar/cloudgateway/CloudGatewayApplication.java
@@ -3,9 +3,11 @@ package com.bondar.cloudgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableHystrix
 public class CloudGatewayApplication {
 
     public static void main(String[] args) {
